ae3da03e8101ac30ad141472ee1eec0a08463798
LexaMart <lexamart1708@gmail.com>
2022-11-04 11:02:33 +0300

    Add user cart form template

--- src/components/SubmitOrderForm/index.tsx
+++ src/components/SubmitOrderForm/index.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.scss';
 
 export const SubmitOrderForm = ({ closeFunc }: { closeFunc: Function }) => {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [addInfo, setAddInfo] = useState("");
+
+
     const closeForm = (event: React.MouseEvent<HTMLDivElement>) => {
         event.stopPropagation();
         event.preventDefault();
         closeFunc(false);
     }
+
+    const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        switch (event.target.id) {
+            case "name":
+                setName(event.target.value);
+                break;
+            case "phone":
+                setPhone(event.target.value);
+                break;
+            default:
+                break
+        }
+    }
+    const onTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setAddInfo(event.target.value)
+    }
+
+
+
     return (
         <div onClick={closeForm} className="shadow">
-            <div className="form">
+            <div onClick={(e) => e.stopPropagation()} className="form">
                 <div className="form__title">Personal Data</div>
                 <div className="form__inputs">
-                    <input className="form__inputs__input" />
+                    <input id="name" value={name} onChange={onInputChange} className="form__inputs__input" placeholder="Your Name" />
+                    <input id="phone" value={phone} onChange={onInputChange} className="form__inputs__input" placeholder="Your Phone" />
+                    <textarea id="add-info" value={addInfo} onChange={onTextAreaChange} className="form__inputs__text" placeholder="Additional Info" />
                 </div>
+                <div className={"form__btns"}><div className={ name && phone ? "form__btns__btn" : "form__btns__btn disabled-btn"}>Submit Order</div></div>
             </div>
         </div>
     )
